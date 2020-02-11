e45e9efb8f0d63ce5872ff66091079e09858e637
YannisMarketakis <yannismarketakis@gmail.com>
2020-02-11 14:37:43 +0200

    transform all resources from Marburg

--- src/main/java/com/smartupds/etlcontroller/etl/controller/Resources.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/Resources.java
@@ -15,6 +15,7 @@ public class Resources {
     public static final String FOLDER_INPUT_FETCHED_HERTZIANA=FOLDER_INPUT_FETCHED+"/"+Resources.HERTZIANA;
     public static final String FOLDER_INPUT_NORMALIZED_HERTZIANA=FOLDER_INPUT_NORMALIZED+"/"+Resources.HERTZIANA;
     public static final String FOLDER_INPUT_FETCHED_VILLA_I_TATTI=FOLDER_INPUT_FETCHED+"/"+Resources.VILLA_I_TATTI;
+    public static final String FOLDER_INPUT_FETCHED_VILLA_I_TATTI_FOTOINDEX=FOLDER_INPUT_FETCHED_VILLA_I_TATTI+"/"+Resources.FOTOINDEX;
     public static final String FOLDER_INPUT_FETCHED_FRICK=FOLDER_INPUT_FETCHED+"/"+Resources.FRICK;
     public static final String FOLDER_INPUT_FETCHED_ZERI=FOLDER_INPUT_FETCHED+"/"+Resources.ZERI;
     public static final String FOLDER_INPUT_FETCHED_ZERI_ARTWORKS=FOLDER_INPUT_FETCHED_ZERI+"/"+Resources.ARTWORKS;
@@ -30,18 +31,33 @@ public class Resources {
     public static final String FOLDER_OUTPUT=FOLDER_WORKSPACE+"/"+"3_Output";
     public static final String FOLDER_OUTPUT_TRANSFORMED=FOLDER_OUTPUT+"/"+"1_Transformed";
     public static final String FOLDER_OUTPUT_TRANSFORMED_VILLA_I_TATTI=FOLDER_OUTPUT_TRANSFORMED+"/"+Resources.VILLA_I_TATTI;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_VILLA_I_TATTI_FOTOINDEX=FOLDER_OUTPUT_TRANSFORMED_VILLA_I_TATTI+"/"+Resources.FOTOINDEX;
     public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA=FOLDER_OUTPUT_TRANSFORMED+"/"+Resources.HERTZIANA;
     public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.ARTWORKS;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL2=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.ARTWORKS+"_lvl2";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL3=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.ARTWORKS+"_lvl3";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL4=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.ARTWORKS+"_lvl4";
     public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_PHOTOGRAPHS=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.PHOTOGRAPHS;
-    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_LVL2=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.LVL2;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.BUILTWORKS;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL2=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.BUILTWORKS+"_lvl2";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL3=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.BUILTWORKS+"_lvl3";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL4=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.BUILTWORKS+"_lvl4";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ACTORS=FOLDER_OUTPUT_TRANSFORMED_HERTZIANA+"/"+Resources.ACTORS;
     public static final String FOLDER_OUTPUT_TRANSFORMED_FRICK=FOLDER_OUTPUT_TRANSFORMED+"/"+Resources.FRICK;
     public static final String FOLDER_OUTPUT_TRANSFORMED_ZERI=FOLDER_OUTPUT_TRANSFORMED+"/"+Resources.ZERI;
     public static final String FOLDER_OUTPUT_TRANSFORMED_ZERI_ARTWORKS=FOLDER_OUTPUT_TRANSFORMED_ZERI+"/"+Resources.ARTWORKS;
     public static final String FOLDER_OUTPUT_TRANSFORMED_ZERI_PHOTOGRAPHS=FOLDER_OUTPUT_TRANSFORMED_ZERI+"/"+Resources.PHOTOGRAPHS;
     public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG=FOLDER_OUTPUT_TRANSFORMED+"/"+Resources.MARBURG;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_ACTORS=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.ACTORS;
     public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.ARTWORKS;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL2=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.ARTWORKS+"_lvl2";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL3=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.ARTWORKS+"_lvl3";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL4=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.ARTWORKS+"_lvl4";
     public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_PHOTOGRAPHS=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.PHOTOGRAPHS;
-    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_L2V=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.L2V;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.BUILTWORKS;
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL2=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.BUILTWORKS+"_lvl2";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL3=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.BUILTWORKS+"_lvl3";
+    public static final String FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL4=FOLDER_OUTPUT_TRANSFORMED_MARBURG+"/"+Resources.BUILTWORKS+"_lvl4";
     public static final String FOLDER_OUTPUT_NORMALIZED=FOLDER_OUTPUT+"/"+"2_Normalized";
     
     /* Sources names */
@@ -50,18 +66,40 @@ public class Resources {
     public static final String ZERI="Zeri";
     public static final String FRICK="Frick";
     public static final String VILLA_I_TATTI="VillaITatti";
+    public static final String FOTOINDEX="FotoIndex";
     
     /* Entities names */
+    public static final String ACTORS="actors";
+    public static final String ARTIST="Artist";
     public static final String ARTWORKS="artworks";
+    public static final String BUILTWORKS="builtworks";
+    public static final String COLLECTION="Collection";
+    public static final String INSTITUTION="Institution";
+    public static final String PHOTOGRAPH="Photograph";
     public static final String PHOTOGRAPHS="photographs";
     public static final String L2V="l2v";
     public static final String LVL2="lvl2";
+    public static final String WORK="Work";
     
     /* Mapping resources */
     public static final String MAPPINGS_VILLA_I_TATTI=FOLDER_MAPPINGS+"/"+"mappings-itatti.x3ml";
+    public static final String MAPPINGS_VILLA_I_TATTI_FOTOINDEX_ARTIST=FOLDER_MAPPINGS+"/"+"mappings-villaitatti-artist.x3ml";
+    public static final String MAPPINGS_VILLA_I_TATTI_FOTOINDEX_COLLECTION=FOLDER_MAPPINGS+"/"+"mappings-villaitatti-collection.x3ml";
+    public static final String MAPPINGS_VILLA_I_TATTI_FOTOINDEX_INSTITUTION=FOLDER_MAPPINGS+"/"+"mappings-villaitatti-institution.x3ml";
+    public static final String MAPPINGS_VILLA_I_TATTI_FOTOINDEX_PHOTO=FOLDER_MAPPINGS+"/"+"mappings-villaitatti-photo.x3ml";
+    public static final String MAPPINGS_VILLA_I_TATTI_FOTOINDEX_WORK=FOLDER_MAPPINGS+"/"+"mappings-villaitatti-work.x3ml";
     public static final String GENERATOR_POLICY_VILLA_I_TATTI=FOLDER_MAPPINGS+"/"+"generator-policy-itatti.xml";
+    public static final String GENERATOR_POLICY_VILLA_I_TATTI_FOTOINDEX=FOLDER_MAPPINGS+"/"+"generator-policy-fotoindex-itatti.xml";
     public static final String MAPPINGS_HERTZIANA_ALL=FOLDER_MAPPINGS+"/"+"mappings-hertziana.x3ml";
+    public static final String MAPPINGS_HERTZIANA_ACTORS=FOLDER_MAPPINGS+"/"+"mappings-hertziana-actors.x3ml";
     public static final String MAPPINGS_HERTZIANA_ARTWORKS=FOLDER_MAPPINGS+"/"+"mappings-hertziana-artworks.x3ml";
+    public static final String MAPPINGS_HERTZIANA_ARTWORKS_LVL2=FOLDER_MAPPINGS+"/"+"mappings-hertziana-artworks-lvl2.x3ml";
+    public static final String MAPPINGS_HERTZIANA_ARTWORKS_LVL3=FOLDER_MAPPINGS+"/"+"mappings-hertziana-artworks-lvl3.x3ml";
+    public static final String MAPPINGS_HERTZIANA_ARTWORKS_LVL4=FOLDER_MAPPINGS+"/"+"mappings-hertziana-artworks-lvl4.x3ml";
+    public static final String MAPPINGS_HERTZIANA_BUILTWORKS=FOLDER_MAPPINGS+"/"+"mappings-hertziana-builtworks.x3ml";
+    public static final String MAPPINGS_HERTZIANA_BUILTWORKS_LVL2=FOLDER_MAPPINGS+"/"+"mappings-hertziana-builtworks-lvl2.x3ml";
+    public static final String MAPPINGS_HERTZIANA_BUILTWORKS_LVL3=FOLDER_MAPPINGS+"/"+"mappings-hertziana-builtworks-lvl3.x3ml";
+    public static final String MAPPINGS_HERTZIANA_BUILTWORKS_LVL4=FOLDER_MAPPINGS+"/"+"mappings-hertziana-builtworks-lvl4.x3ml";
     public static final String MAPPINGS_HERTZIANA_PHOTOGRAPHS=FOLDER_MAPPINGS+"/"+"mappings-hertziana-photographs.x3ml";
     public static final String MAPPINGS_HERTZIANA_LVL2=FOLDER_MAPPINGS+"/"+"mappings-hertziana-lvl2.x3ml";
     public static final String GENERATOR_POLICY_HERTZIANA=FOLDER_MAPPINGS+"/"+"generator-policy-hertziana.xml";
@@ -70,7 +108,15 @@ public class Resources {
     public static final String MAPPINGS_ZERI_ARTWORKS=FOLDER_MAPPINGS+"/"+"mappings-zeri-artworks.x3ml";
     public static final String MAPPINGS_ZERI_PHOTOGRAPHS=FOLDER_MAPPINGS+"/"+"mappings-zeri-photographs.x3ml";
     public static final String GENERATOR_POLICY_ZERI=FOLDER_MAPPINGS+"/"+"generator-policy-zeri.xml";
+    public static final String MAPPINGS_MARBURG_ACTORS=FOLDER_MAPPINGS+"/"+"mappings-marburg-actors.x3ml";
     public static final String MAPPINGS_MARBURG_ARTWORKS=FOLDER_MAPPINGS+"/"+"mappings-marburg-artworks.x3ml";
+    public static final String MAPPINGS_MARBURG_ARTWORKS_LVL2=FOLDER_MAPPINGS+"/"+"mappings-marburg-artworks-lvl2.x3ml";
+    public static final String MAPPINGS_MARBURG_ARTWORKS_LVL3=FOLDER_MAPPINGS+"/"+"mappings-marburg-artworks-lvl3.x3ml";
+    public static final String MAPPINGS_MARBURG_ARTWORKS_LVL4=FOLDER_MAPPINGS+"/"+"mappings-marburg-artworks-lvl4.x3ml";
+    public static final String MAPPINGS_MARBURG_BUILTWORKS=FOLDER_MAPPINGS+"/"+"mappings-marburg-builtworks.x3ml";
+    public static final String MAPPINGS_MARBURG_BUILTWORKS_LVL2=FOLDER_MAPPINGS+"/"+"mappings-marburg-builtworks-lvl2.x3ml";
+    public static final String MAPPINGS_MARBURG_BUILTWORKS_LVL3=FOLDER_MAPPINGS+"/"+"mappings-marburg-builtworks-lvl3.x3ml";
+    public static final String MAPPINGS_MARBURG_BUILTWORKS_LVL4=FOLDER_MAPPINGS+"/"+"mappings-marburg-builtworks-lvl4.x3ml";
     public static final String MAPPINGS_MARBURG_PHOTOGRAPHS=FOLDER_MAPPINGS+"/"+"mappings-marburg-photographs.x3ml";
     public static final String MAPPINGS_MARBURG_L2V=FOLDER_MAPPINGS+"/"+"mappings-marburg-l2v.x3ml";
     public static final String GENERATOR_POLICY_MARBURG=FOLDER_MAPPINGS+"/"+"generator-policy-marburg.xml";
--- src/main/java/com/smartupds/etlcontroller/etl/controller/impl/marburg/MarburgTransformer.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/impl/marburg/MarburgTransformer.java
@@ -36,28 +36,105 @@ public class MarburgTransformer implements Transformer{
             }
             Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworks");
             log.info("FINISH: Transform Artworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworks"));
+            
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl2");
+            log.info("START: Transform Artworks LVL2 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_ARTWORKS_LVL2),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL2));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl2");
+            log.info("FINISH: Transform Artworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl2"));
+            
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl3");
+            log.info("START: Transform Artworks LVL3 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_ARTWORKS_LVL3),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL3));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl3");
+            log.info("FINISH: Transform Artworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl3"));
+            
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl4");
+            log.info("START: Transform Artworks LVL4 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_ARTWORKS_LVL4),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_ARTWORKS_LVL4));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl4");
+            log.info("FINISH: Transform Artworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.artworkslvl4"));
 
-//            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs");
-//            log.info("START: Transform Photographs from Marburg");
-//            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
-//                this.transformFile(file,
-//                               new File(Resources.MAPPINGS_MARBURG_PHOTOGRAPHS),
-//                               new File(Resources.GENERATOR_POLICY_MARBURG),
-//                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_PHOTOGRAPHS));
-//            }
-//            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs");
-//            log.info("FINISH: Transform Photographs from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs"));
-//
-//            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.l2v");
-//            log.info("START: Transform L2V from Marburg");
-//            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
-//                this.transformFile(file,
-//                               new File(Resources.MAPPINGS_MARBURG_L2V),
-//                               new File(Resources.GENERATOR_POLICY_MARBURG),
-//                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_L2V));
-//            }
-//            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.l2v");
-//            log.info("FINISH: Transform L2v from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.l2v"));
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworks");
+            log.info("START: Transform Builtworks from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_BUILTWORKS),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworks");
+            log.info("FINISH: Transform Builtworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworks"));
+
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl2");
+            log.info("START: Transform Builtworks LVL2 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_BUILTWORKS_LVL2),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL2));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl2");
+            log.info("FINISH: Transform Builtworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl2"));
+
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl3");
+            log.info("START: Transform Builtworks LVL3 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_BUILTWORKS_LVL3),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL3));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl3");
+            log.info("FINISH: Transform Builtworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl3"));
+
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl4");
+            log.info("START: Transform Builtworks LVL4 from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_BUILTWORKS_LVL4),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_BUILTWORKS_LVL4));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl4");
+            log.info("FINISH: Transform Builtworks from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.builtworkslvl4"));
+            
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs");
+            log.info("START: Transform Photographs from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_PHOTOGRAPHS),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_PHOTOGRAPHS));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs");
+            log.info("FINISH: Transform Photographs from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.photographs"));
+
+            Timer.start("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.actors");
+            log.info("START: Transform Actors from Marburg");
+            for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG), null, true)){
+                this.transformFile(file,
+                               new File(Resources.MAPPINGS_MARBURG_ACTORS),
+                               new File(Resources.GENERATOR_POLICY_MARBURG),
+                               new File(Resources.FOLDER_OUTPUT_TRANSFORMED_MARBURG_ACTORS));
+            }
+            Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.actors");
+            log.info("FINISH: Transform Actors from Marburg in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.marburg.marburgtransformer.transform.actors"));
         }catch(FileNotFoundException ex){
             log.error("An error occured while transforming resources",ex);
             throw new ETLGenericException("An error occured while transforming resources",ex);
@@ -71,12 +148,12 @@ public class MarburgTransformer implements Transformer{
      * @param generatorPolicyFile the generator policy file
      * @param outputFolder the folder where the transformed contents will be exported. */
     private void transformFile(File inputFile, File mappingsFile, File generatorPolicyFile, File outputFolder) throws FileNotFoundException{
-        File outputFile=new File(outputFolder.getAbsolutePath()+"/"+inputFile.getName().replace(".xml", ".rdf"));
+        File outputFile=new File(outputFolder.getAbsolutePath()+"/"+inputFile.getName().replace(".xml", ".n3"));
         log.debug("Transforming resource "+inputFile.getAbsolutePath());
         X3MLEngine engine=X3MLEngine.load(new FileInputStream(mappingsFile));
         Generator policy=X3MLGeneratorPolicy.load(new FileInputStream(generatorPolicyFile), X3MLGeneratorPolicy.createUUIDSource(-1));
         X3MLEngine.Output output = engine.execute(document(inputFile), policy);
-        output.writeXML(new PrintStream(outputFile));
+        output.write(new PrintStream(outputFile),"application/n-triples");
     }
     
     private static Element document(File file) {
