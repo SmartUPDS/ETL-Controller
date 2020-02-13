3627869280415af5901459e9b2e9e54d69aa2b1f
YannisMarketakis <yannismarketakis@gmail.com>
2020-02-13 14:24:23 +0200

    uncommented useful code and removed deletion entry (it was used only for debugging)

--- src/main/java/com/smartupds/etlcontroller/etl/controller/impl/zeri/ZeriTransformer.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/impl/zeri/ZeriTransformer.java
@@ -29,16 +29,16 @@ public class ZeriTransformer implements Transformer {
         Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.artworks");
         log.info("FINISH: Transform artworks from Zeri in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.artworks"));
         
-//        log.info("START: Transform Photographs from Zeri");
-//        Timer.start("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.photographs");
-//        for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_ZERI_PHOTOGRAPHS), null, true)){
-//            this.transformFile(file,
-//                           new File(Resources.MAPPINGS_ZERI_PHOTOGRAPHS),
-//                           new File(Resources.GENERATOR_POLICY_ZERI),
-//                           new File(Resources.FOLDER_OUTPUT_TRANSFORMED_ZERI_PHOTOGRAPHS));
-//        }
-//        Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.photographs");
-//        log.info("FINISH: Transform photographs from Zeri in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.photographs"));
+        log.info("START: Transform Photographs from Zeri");
+        Timer.start("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.photographs");
+        for(File file: FileUtils.listFiles(new File(Resources.FOLDER_INPUT_NORMALIZED_ZERI_PHOTOGRAPHS), null, true)){
+            this.transformFile(file,
+                           new File(Resources.MAPPINGS_ZERI_PHOTOGRAPHS),
+                           new File(Resources.GENERATOR_POLICY_ZERI),
+                           new File(Resources.FOLDER_OUTPUT_TRANSFORMED_ZERI_PHOTOGRAPHS));
+        }
+        Timer.stop("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.transform.photographs");
+        log.info("FINISH: Transform photographs from Zeri in "+Timer.reportHumanFriendly("com.smartupds.etlcontroller.etl.controller.impl.zeri.zeritransformer.photographs"));
     }
     
     /** Transforms the given resources using X3ML engine.
@@ -48,15 +48,14 @@ public class ZeriTransformer implements Transformer {
      * @param generatorPolicyFile the generator policy file
      * @param outputFolder the folder where the transformed contents will be exported. */
     private void transformFile(File inputFile, File mappingsFile, File generatorPolicyFile, File outputFolder){
-        File outputFile=new File(outputFolder.getAbsolutePath()+"/"+inputFile.getName().replace(".xml", ".rdf"));
+        File outputFile=new File(outputFolder.getAbsolutePath()+"/"+inputFile.getName().replace(".xml", ".n3"));
         log.debug("Transforming resource "+inputFile.getAbsolutePath());
         X3MLEngineFactory.create()
                          .withInputFiles(inputFile)
                          .withMappings(mappingsFile)
                          .withGeneratorPolicy(generatorPolicyFile)
-                         .withOutput(outputFile, X3MLEngineFactory.OutputFormat.RDF_XML)
+                         .withOutput(outputFile, X3MLEngineFactory.OutputFormat.NTRIPLES)
                          .execute();
-        inputFile.delete(); //TO REMOVE THIS AFTER DEBUGGING
     }
     
     public static ZeriTransformer create(){
