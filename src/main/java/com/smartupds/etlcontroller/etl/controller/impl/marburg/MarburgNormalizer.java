a19b416e084ec66f10a298711e7f8da5b2d97bd1
mafragias <mafragias@hotmail.com>
2020-09-23 15:20:20 +0300

    Update MarburgNormalizer.java

--- src/main/java/com/smartupds/etlcontroller/etl/controller/impl/marburg/MarburgNormalizer.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/impl/marburg/MarburgNormalizer.java
@@ -8,10 +8,13 @@ import com.smartupds.xmlsplit.Splitter;
 import gr.forth.ics.isl.timer.Timer;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
@@ -19,6 +22,7 @@ import lombok.extern.log4j.Log4j;
 import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import split.ElementsSplit;
@@ -51,6 +55,7 @@ public class MarburgNormalizer implements Normalizer{
                                                 "a8498");
         try{
             this.normalizeSyntax(new File(Resources.FOLDER_INPUT_NORMALIZED_MARBURG),elementsList,"&");
+            
         }catch(NormalizerException | IOException ex){
             log.error("An error occured while normalizing input resources",ex);
             throw new ETLGenericException("An error occured while normalizing input resources",ex);
@@ -103,6 +108,7 @@ public class MarburgNormalizer implements Normalizer{
             doc=normalizeYear(doc, "a5064");
             doc=normalizeYear(doc, "a8494");
             doc=identifySource(doc, "a30nr");
+            doc=normalizeType(doc,"obj","a5220");
             ElementsSplit.exportXmlDocument(doc, new File(folderName+"/"+filename.replace(".xml","")+"_cleaned"+".xml")); 
             FileUtils.deleteQuietly(file);  //Seems that it doesn't work
         }
@@ -132,6 +138,87 @@ public class MarburgNormalizer implements Normalizer{
         return doc;
     }
     
+    private Document normalizeType(Document doc,String elementName, String afterElement) {
+        log.info("START: Normalize Type");
+        NodeList parentNodes=doc.getElementsByTagName(elementName);
+        for(int i=0;i<parentNodes.getLength();i++){
+            NodeList childNodes = parentNodes.item(i).getChildNodes();
+            String text = "";
+            ArrayList<Node> nodesToRemove = new ArrayList<>();
+            Node baseNode = null;
+            for (int j=0;j<childNodes.getLength();j++){
+                if(childNodes.item(j).getNodeName().equals(afterElement)){
+                    if(childNodes.item(j).getChildNodes().getLength()==1){
+                        text = text + childNodes.item(j).getChildNodes().item(0).getTextContent();
+                        baseNode = childNodes.item(j);
+                        nodesToRemove.add(baseNode);
+                    }
+                }
+            }
+            for(Node child:nodesToRemove)
+                parentNodes.item(i).removeChild(child);
+            if (!text.equals("")){
+                Matcher a520nr = Pattern.compile("([a-zA-Z]+[0-9-]+[\\s]*\\([a-zA-Z]*\\))").matcher(text);
+                Matcher typeAndId = Pattern.compile("([a-zA-Z]+)([0-9]+)[\\s]*[a-z]").matcher(text);
+                Matcher a520a = Pattern.compile("[^\\||\\(|\\\\]\\b([a-zA-Z]+)\\b[^\\||\\)]").matcher(text);
+                Matcher a520m = Pattern.compile("([\\|]*[0-9]+[\\\\]*[a-zA-Z]+)+").matcher(text);
+                String a5220 = "";
+                String a520s = "";
+                if (typeAndId.find()){
+                    a5220 = typeAndId.group(1);
+                    a520s = typeAndId.group(2);
+                }
+                String a520aStr = "";
+                if (a520a.find())
+                    a520aStr = a520a.group(1);
+                String a010gn = "";
+                while (a520nr.find()){
+                    a010gn += a520nr.group(1) + " & " ;
+                }
+                a010gn = a010gn.substring(0,a010gn.lastIndexOf("&"));
+                String a520mStr = "";
+                if (a520m.find())
+                    a520mStr = a520m.group();
+                if (baseNode!=null){
+                    Node newNode = baseNode.cloneNode(false);
+                    newNode.setTextContent(a5220);
+                    Node id = baseNode.cloneNode(false);
+                    doc.renameNode(id, null, "a520s");
+                    id.setTextContent(a520s);
+                    ((Element)id).setAttribute("edp:augmented", "the::"+a520s);
+                    newNode.appendChild(id);
+                    if (!a010gn.equals("")){
+                        Node sameAs = baseNode.cloneNode(false);
+                        doc.renameNode(sameAs, null, "a520nr");
+                        ((Element)sameAs).setAttribute("edp:augmented", "the::"+a520s);
+                        Node link = baseNode.cloneNode(false);
+                        doc.renameNode(link, null, "a010gn");
+                        link.setTextContent(a010gn);
+                        sameAs.appendChild(link);
+                        newNode.appendChild(sameAs);
+                    }
+                    if (!a520aStr.equals("")){
+                        Node type = baseNode.cloneNode(false);
+                        doc.renameNode(type, null, "a520a");
+                        ((Element)type).setAttribute("edp:augmented", "the::"+a520s);
+                        type.setTextContent(a520aStr);
+                        newNode.appendChild(type);
+                    }
+                    if (!a520mStr.equals("")){
+                        Node info = baseNode.cloneNode(false);
+                        doc.renameNode(info, null, "a520m");
+                        ((Element)info).setAttribute("edp:augmented", "the::"+a520s);
+                        info.setTextContent(a520mStr);
+                        newNode.appendChild(info);
+                    }
+                    parentNodes.item(i).appendChild(newNode);
+                }
+            }
+        }
+        log.info("END: Normalize Type");
+        return doc;
+    }
+        
     private Document identifySource(Document doc, String elementName){
         log.info("START: Identify Source");
         NodeList parentNodes=doc.getElementsByTagName(elementName);
