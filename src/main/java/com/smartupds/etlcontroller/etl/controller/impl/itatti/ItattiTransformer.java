bbee4cab77f41b812a51054dfd23d5ee66d5c007
mafragias <mafragias@hotmail.com>
2021-03-10 16:12:43 +0200

    Update ItattiTransformer.java
    sharedshelf remove custom:noType

--- src/main/java/com/smartupds/etlcontroller/etl/controller/impl/itatti/ItattiTransformer.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/impl/itatti/ItattiTransformer.java
@@ -1,5 +1,10 @@
 package com.smartupds.etlcontroller.etl.controller.impl.itatti;
 
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
 import com.smartupds.etlcontroller.etl.controller.Resources;
 import com.smartupds.etlcontroller.etl.controller.Utils;
 import com.smartupds.etlcontroller.etl.controller.api.Transformer;
@@ -7,7 +12,14 @@ import com.smartupds.etlcontroller.etl.controller.exception.ETLGenericException;
 import gr.forth.ics.isl.timer.Timer;
 import gr.forth.ics.isl.x3ml.X3MLEngineFactory;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.extern.log4j.Log4j;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 
 /** Transformer class for resources from Villa I Tatti
  *
@@ -16,16 +28,17 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ItattiTransformer implements Transformer {
 
-     @Override
+    @Override
     public void transformResources() throws ETLGenericException {
         Timer.start(ItattiTransformer.class.getCanonicalName()+".sharedshelf");
         log.info("START: Transform SharedShelf data from Villa I Tatti");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_VILLA_I_TATTI_SHAREDSHELF).listFiles()){
-            Utils.transformFile(file,
-                                new File(Resources.MAPPINGS_VILLA_I_TATTI_SHAREDSHELF_ALL),
-                                new File(Resources.GENERATOR_POLICY_VILLA_I_TATTI_SHAREDSHELF),
-                                new File(Resources.FOLDER_OUTPUT_TRANSFORMED_VILLA_I_TATTI_SHAREDSHELF), 
-                                X3MLEngineFactory.OutputFormat.TRIG);
+            String filename = Utils.transformFile(  file,
+                                                    new File(Resources.MAPPINGS_VILLA_I_TATTI_SHAREDSHELF_ALL),
+                                                    new File(Resources.GENERATOR_POLICY_VILLA_I_TATTI_SHAREDSHELF),
+                                                    new File(Resources.FOLDER_OUTPUT_TRANSFORMED_VILLA_I_TATTI_SHAREDSHELF), 
+                                                    X3MLEngineFactory.OutputFormat.TRIG);
+            this.removeTypes(filename);
         }
         Timer.stop(ItattiTransformer.class.getCanonicalName()+".sharedshelf");
         log.info("FINISH: Transform SharedShelf data from Villa I Tatti in "+Timer.reportHumanFriendly(ItattiTransformer.class.getCanonicalName()+".sharedshelf"));
@@ -113,6 +126,20 @@ public class ItattiTransformer implements Transformer {
     
     public static ItattiTransformer create(){
         return new ItattiTransformer();
+    }
+
+    private void removeTypes(String filename) {
+        try {
+            Dataset dataset = RDFDataMgr.loadDataset(filename);
+            dataset.listNames().forEachRemaining(graph -> {
+                Model model = dataset.getNamedModel(graph);
+                Resource rsc = model.createResource(Resources.NO_TYPE);
+                model.removeAll(null, RDF.type,(RDFNode) rsc);
+            });
+            RDFDataMgr.write(new FileOutputStream(filename), dataset, Lang.TRIG);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ItattiTransformer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
