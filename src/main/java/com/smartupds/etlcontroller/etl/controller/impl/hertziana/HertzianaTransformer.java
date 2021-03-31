535aaf38c18014937673688853734ec6a0a92fc2
mafragias <mafragias@hotmail.com>
2021-03-31 14:38:23 +0300

    Update HertzianaTransformer.java

--- src/main/java/com/smartupds/etlcontroller/etl/controller/impl/hertziana/HertzianaTransformer.java
+++ src/main/java/com/smartupds/etlcontroller/etl/controller/impl/hertziana/HertzianaTransformer.java
@@ -21,11 +21,13 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".artworks");
         log.info("START: Transform artworks from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_ARTWORKS),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
+
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".artworks");
         log.info("FINISH: Transform artworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".artworks"));
@@ -33,11 +35,13 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".artworks-lv2");
         log.info("START: Transform artworks LVL2 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_ARTWORKS_LVL2),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL2), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
+
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".artworks-lv2");
         log.info("FINISH: Transform artworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".artworks-lv2"));
@@ -45,11 +49,13 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".artworks-lv3");
         log.info("START: Transform artworks LVL3 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_ARTWORKS_LVL3),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL3), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
+
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".artworks-lv3");
         log.info("FINISH: Transform artworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".artworks-lv3"));
@@ -57,11 +63,13 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".artworks-lv4");
         log.info("START: Transform artworks LVL4 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_ARTWORKS_LVL4),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ARTWORKS_LVL4), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
+
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".artworks-lv4");
         log.info("FINISH: Transform artworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".artworks-lv4"));
@@ -69,11 +77,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".builtworks");
         log.info("START: Transform Builtworks from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_BUILTWORKS),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".builtworks");
         log.info("FINISH: Transform Builtworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".builtworks"));
@@ -81,11 +90,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv2");
         log.info("START: Transform Builtworks LVL2 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_BUILTWORKS_LVL2),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL2), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv2");
         log.info("FINISH: Transform Builtworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv2"));
@@ -93,11 +103,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv3");
         log.info("START: Transform Builtworks LVL3 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_BUILTWORKS_LVL3),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL3), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv3");
         log.info("FINISH: Transform Builtworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv3"));
@@ -105,11 +116,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv4");
         log.info("START: Transform Builtworks LVL4 from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_BUILTWORKS_LVL4),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_BUILTWORKS_LVL4), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv4");
         log.info("FINISH: Transform Builtworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".builtworks-lv4"));
@@ -117,11 +129,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".photographs");
         log.info("START: Transform photographs from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_PHOTOGRAPHS),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_PHOTOGRAPHS), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".photographs");
         log.info("FINISH: Transform photographs from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".photographs"));
@@ -129,11 +142,12 @@ public class HertzianaTransformer implements Transformer {
         Timer.start(HertzianaTransformer.class.getCanonicalName()+".actors");
         log.info("START: Transform Actors from Hertziana");
         for(File file: new File(Resources.FOLDER_INPUT_NORMALIZED_HERTZIANA).listFiles()){
-            Utils.transformFile(file,
+            String filename =   Utils.transformFile(file,
                                 new File(Resources.MAPPINGS_HERTZIANA_ACTORS),
                                 new File(Resources.GENERATOR_POLICY_HERTZIANA),
                                 new File(Resources.FOLDER_OUTPUT_TRANSFORMED_HERTZIANA_ACTORS), 
                                 X3MLEngineFactory.OutputFormat.TRIG);
+            Utils.removeTypes(filename);
         }
         Timer.stop(HertzianaTransformer.class.getCanonicalName()+".actors");
         log.info("FINISH: Transform Builtworks from Hertziana in "+Timer.reportHumanFriendly(HertzianaTransformer.class.getCanonicalName()+".actors"));
